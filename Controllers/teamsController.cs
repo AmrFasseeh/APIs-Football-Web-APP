6f008c347f5fdd49fb4fec834592ca9d3c777712
Amr Fasseeh <amr_m_fasseeh@hotmail.com>
2020-12-21 04:05:40 +0200

    Edited Teams Controller

--- Controllers/teamsController.cs
+++ Controllers/teamsController.cs
@@ -19,6 +19,56 @@ namespace APIs_FinalProject.Controllers
         // GET: api/teams
         public IQueryable<team> Getteams()
         {
+            var allteams = db.teams.ToList();
+
+            foreach (var item in allteams)
+            {
+                if (item.TeamMatches.Count != 0)
+                {
+                    var matches = item.TeamMatches.Where(a => a.team_id == item.team_id);
+                    item.points = 0;
+                    item.wins = 0;
+                    item.loss = 0;
+                    item.draws = 0;
+                    foreach (var m in matches)
+                    {
+                        if (m.home_Away == "home" && m.match.status == "Finished")
+                        {
+                            if (m.match.team1_score > m.match.team2_score)
+                            {
+                                item.points += 3;
+                                item.wins++;
+                            }
+                            else if (m.match.team1_score < m.match.team2_score)
+                            {
+                                item.loss++;
+                            }
+                            else if (m.match.team1_score == m.match.team2_score)
+                            {
+                                item.points++;
+                                item.draws++;
+                            }
+                        }
+                        else if (m.home_Away == "away" && m.match.status == "Finished")
+                        {
+                            if (m.match.team1_score < m.match.team2_score)
+                            {
+                                item.points += 3;
+                                item.wins++;
+                            }
+                            else if (m.match.team1_score > m.match.team2_score)
+                            {
+                                item.loss++;
+                            }
+                            else if (m.match.team1_score == m.match.team2_score)
+                            {
+                                item.points++;
+                                item.draws++;
+                            }
+                        }
+                    }
+                }
+            }
             return db.teams;
         }
 
@@ -27,6 +77,56 @@ namespace APIs_FinalProject.Controllers
         public IHttpActionResult Getteam(int id)
         {
             team team = db.teams.Find(id);
+            var allteams = db.teams.ToList();
+
+            foreach (var item in allteams)
+            {
+                if (item.TeamMatches.Count != 0)
+                {
+                    var matches = item.TeamMatches.Where(a => a.team_id == item.team_id);
+                    item.points = 0;
+                    item.wins = 0;
+                    item.loss = 0;
+                    item.draws = 0;
+                    foreach (var m in matches)
+                    {
+                        if (m.home_Away == "home" && m.match.status == "Finished")
+                        {
+                            if (m.match.team1_score > m.match.team2_score)
+                            {
+                                item.points += 3;
+                                item.wins++;
+                            }
+                            else if (m.match.team1_score < m.match.team2_score)
+                            {
+                                item.loss++;
+                            }
+                            else if (m.match.team1_score == m.match.team2_score)
+                            {
+                                item.points++;
+                                item.draws++;
+                            }
+                        }
+                        else if (m.home_Away == "away" && m.match.status == "Finished")
+                        {
+                            if (m.match.team1_score < m.match.team2_score)
+                            {
+                                item.points += 3;
+                                item.wins++;
+                            }
+                            else if (m.match.team1_score > m.match.team2_score)
+                            {
+                                item.loss++;
+                            }
+                            else if (m.match.team1_score == m.match.team2_score)
+                            {
+                                item.points++;
+                                item.draws++;
+                            }
+                        }
+                    }
+                }
+            }
             if (team == null)
             {
                 return NotFound();
@@ -40,6 +140,56 @@ namespace APIs_FinalProject.Controllers
         public IHttpActionResult GetteamsPerLeague(int id)
         {
             var team = db.teams.Where(n =>n.league_id == id);
+            var allteams = db.teams.ToList();
+
+            foreach (var item in allteams)
+            {
+                if (item.TeamMatches.Count != 0)
+                {
+                    var matches = item.TeamMatches.Where(a => a.team_id == item.team_id);
+                    item.points = 0;
+                    item.wins = 0;
+                    item.loss = 0;
+                    item.draws = 0;
+                    foreach (var m in matches)
+                    {
+                        if (m.home_Away == "home" && m.match.status == "Finished")
+                        {
+                            if (m.match.team1_score > m.match.team2_score)
+                            {
+                                item.points += 3;
+                                item.wins++;
+                            }
+                            else if (m.match.team1_score < m.match.team2_score)
+                            {
+                                item.loss++;
+                            }
+                            else if (m.match.team1_score == m.match.team2_score)
+                            {
+                                item.points++;
+                                item.draws++;
+                            }
+                        }
+                        else if (m.home_Away == "away" && m.match.status == "Finished")
+                        {
+                            if (m.match.team1_score < m.match.team2_score)
+                            {
+                                item.points += 3;
+                                item.wins++;
+                            }
+                            else if (m.match.team1_score > m.match.team2_score)
+                            {
+                                item.loss++;
+                            }
+                            else if (m.match.team1_score == m.match.team2_score)
+                            {
+                                item.points++;
+                                item.draws++;
+                            }
+                        }
+                    }
+                }
+            }
             if (team == null)
             {
                 return NotFound();
